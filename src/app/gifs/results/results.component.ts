6c895fd669b52cdcdee3d16e190857316bc84b4f
Pau <36511149+ptaltavull@users.noreply.github.com>
2022-10-09 11:11:54 +0200

    fav and download buttons

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
+  providers: [AuthService]
 })
 export class ResultsComponent {
+
+  public user$: Observable<any> = this.authSvc.afAuth.user;
 
   get results() {
     return this.gifsService.results;
   }
 
-  constructor(private gifsService: GifsService) { }
+  constructor(private gifsService: GifsService, private authSvc: AuthService) { }
 
 }
